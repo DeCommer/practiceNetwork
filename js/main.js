ef460247ce92b0c4b69fc56953840d063668f5d2
Joseph DeCommer <jdecommer@gmail.com>
2023-03-01 08:08:02 -0500

    added and removed files

--- js/main.js
+++ js/main.js
@@ -5,18 +5,11 @@ window.addEventListener('scroll', checkSection)
 checkSection();
 
 function checkSection() {
-    const triggerBottom = window.innerHeight / 5 * 4;
     const triggerTop = window.innerHeight / 4;
     
 
     sections.forEach(section => {
         const sectionTop = section.getBoundingClientRect().top;
-
-        if(sectionTop < triggerBottom) {
-            section.classList.add('show');
-        } else {
-            section.classList.remove('show');
-        }
 
         if(sectionTop < triggerTop) {
             section.classList.remove('show');
