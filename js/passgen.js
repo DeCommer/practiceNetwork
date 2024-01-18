d9ae796d12c96b9df8b64299b79bc7b5f7fc9dc9
Joseph DeCommer <jdecommer@gmail.com>
2024-01-18 16:36:29 -0500

    password generator added

--- js/passgen.js
+++ js/passgen.js
@@ -2,42 +2,35 @@
 
     'use strict'
 
-    document.getElementById('convert').addEventListener('submit', function(e){
+    document.getElementById('genrate').addEventListener('submit', function(e){
         e.preventDefault();
 
-        const chars = parseFloat(document.getElementById('distance').value); //gets number from textfield
+        const chars = parseFloat(document.getElementById('n_field').value); //gets number from textfield
         const answer = document.getElementById('answer');
 
         if(chars) {
 
-            const array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+//           const conversion = (chars * 1.609344).toFixed(3);
+
+            const alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
             'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
             'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z',
             '0', '1', '2', '3', '4', '5', '6', '7', '8' ,'9',
-            '!', '@', '#', '$', '%', '&', '*', '?', '~']
-    
-            const final_pass = []
+            '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', '\\', ':',
+            ';', '"', "'", '<', '>', ',', '.', '?', '/'];
 
-//           const conversion = (chars * 1.609344).toFixed(3);
-
-            function shuffle(array) {
-                let currentIndex = array.length,  randomIndex;
-            
-                // While there remain elements to shuffle.
-                while (currentIndex > 0) {
-            
-                // Pick a remaining element.
-                randomIndex = Math.floor(Math.random() * currentIndex);
-                currentIndex--;
-            
-                // And swap it with the current element.
-                [array[currentIndex], array[randomIndex]] = [
-                    array[randomIndex], array[currentIndex]];
-                }
-                return array;
+            function generateRandomPassword(n_letters) {
+            let final_pass = [];
+            for (let ch = 1; ch <= n_letters; ch++) {
+            final_pass.push(alpha[Math.floor(Math.random() * alpha.length)]);
+            }
+            // JavaScript arrays have a built-in method to shuffle
+            final_pass.sort(() => Math.random() - 0.5);
+            let final_rand_password = final_pass.join('');
+            return final_rand_password;
             }
 
-            answer.innerHTML = `<h2>${shuffle(array)}</h2>`;
+            answer.innerHTML = `<h2>${generateRandomPassword(chars)}</h2>`;
         } 
 
         else {
