e686928d4210d20c3d650fdbdaea8b6be14f1f33
Joseph DeCommer <jdecommer@gmail.com>
2024-01-22 07:29:31 -0500

    Added guessing game

---
+++ js/guess.js
@@ -0,0 +1,39 @@
+
+const guessBox = document.getElementById("guessBox");
+const remaining = document.getElementById("remaining");
+const result = document.getElementById("result");
+const enterButton = document.getElementById("enterButton");
+
+let secret_number = Math.floor(Math.random() * 100) + 1;
+
+let guesses = 3;
+
+
+function guess(){
+    guesses - 1
+    let userGuess = Number(guessBox.value);
+    console.log(secret_number);   
+    if (isNaN(userGuess)) {
+        result.textContent = "You must enter a number.";   
+    }else if (userGuess == secret_number) {
+        result.textContent = "You got it!"
+    }else if (guesses == 1){
+        remaining.textContent = (`You have: ${guesses - 1} guesses left`);
+        result.textContent = "You lose!"
+    } else if(userGuess > secret_number) {
+        guesses --;
+        remaining.textContent = (`You have: ${guesses} guesses left`);
+        result.textContent = (`${userGuess} is too high`);
+    }else if(userGuess < secret_number) {
+        guesses --;
+        remaining.textContent = (`You have: ${guesses} guesses left`);
+        result.textContent = (`${userGuess} is too low`);
+    }
+
+
+    //reset area
+    document.getElementById('reset').addEventListener('click', () => {
+        location.reload();
+    })
+
+};
