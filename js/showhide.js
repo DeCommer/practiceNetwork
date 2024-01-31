5208c1551b40edb773bea1eac0e67c6507e3bafb
Joseph DeCommer <jdecommer@gmail.com>
2024-01-31 11:55:27 -0500

    Various edits / added about section

--- js/showhide.js
+++ js/showhide.js
@@ -1,13 +1,34 @@
-const hidden = document.querySelector('.hidden');
+// Show / hide JS
+const jsHidden = document.querySelector('.jsHidden');
 const toggleBtn = document.querySelector('.toggleBtn');
 
-
 toggleBtn.addEventListener('click', function hideCode() {
-    if (hidden.style.display === "block") {
-        hidden.style.display = "none";
+    if (jsHidden.style.display === "block") {
+        jsHidden.style.display = "none";
         toggleBtn.textContent = "See the Javascript"
     } else {
-        hidden.style.display = "block";
+        jsHidden.style.display = "block";
         toggleBtn.textContent = "Hide the Javascript"
     }
   });
+
+// Modal
+
+const aboutModal = document.querySelector('.aboutModal');
+const aboutOverlay = document.querySelector('.aboutOverlay');
+
+const openAboutModal = function() {
+    aboutModal.classList.remove('hidden');
+    aboutOverlay.classList.remove('hidden');
+}
+
+const closeAboutModal = function() {
+    aboutModal.classList.add('hidden');
+    aboutOverlay.classList.add('hidden');
+}
+
+document.querySelector('.aboutBtn').addEventListener('click', openAboutModal);
+
+document.querySelector('.closeAboutModal').addEventListener('click', closeAboutModal);
+
+document.querySelector('.aboutOverlay').addEventListener('click', closeAboutModal);
