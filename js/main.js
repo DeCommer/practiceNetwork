26054c93d948d6e083cb513f1464eff4be312bda
Joseph DeCommer <jdecommer@gmail.com>
2024-02-08 07:41:32 -0500

    Daily Practice

--- js/main.js
+++ js/main.js
@@ -1,22 +1,26 @@
-const sections = document.querySelectorAll('.section');
+// const sections = document.querySelectorAll('.section');
 
-window.addEventListener('scroll', checkSection)
+// window.addEventListener('scroll', checkSection)
 
-checkSection();
+// checkSection();
 
-function checkSection() {
-    const triggerTop = window.innerHeight / 4;
+// function checkSection() {
+//     const triggerTop = window.innerHeight / 4;
     
 
-    sections.forEach(section => {
-        const sectionTop = section.getBoundingClientRect().top;
+//     sections.forEach(section => {
+//         const sectionTop = section.getBoundingClientRect().top;
 
-        if(sectionTop < triggerTop) {
-            section.classList.remove('show');
-        }else {
-            section.classList.add('show');
-        }
-    })
-}
+//         if(sectionTop < triggerTop) {
+//             section.classList.remove('show');
+//         }else {
+//             section.classList.add('show');
+//         }
+//     })
+// }
+
+
+
+
 
 
