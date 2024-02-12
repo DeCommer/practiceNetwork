995054b82b53916771945833c9943e8f89667b58
Joseph DeCommer <jdecommer@gmail.com>
2024-02-12 13:40:21 -0500

    added quadratic solver

--- js/quadsolver.js
+++ js/quadsolver.js
@@ -4,41 +4,36 @@ document.querySelector('.solveBtn').addEventListener('click', function() {
     let b = parseFloat(document.getElementById('b').value);
     let c = parseFloat(document.getElementById('c').value);
 
+    let yInt = document.getElementById('yInt');
+    let vertex = document.getElementById('vertex');
+    let zeros = document.getElementById('zeros');
 
-
-    console.log(a, b, c);
+    let vertexFromula = function(a, b, c) {
+        if(a == 0) {
+            return ``
+        } else {
+            const xVert = -b / (2 * a)
+            const yVert = a * (xVert ** 2) + (b * xVert) + c
+            return `${xVert}, ${yVert})`;
+        };
+    };
+    let yIntFormula = function(a, b, c) {
+        return `(0, ${c})`
+    };
+    let zerosFormula = function(a, b, c) {
+        const qePos = (-b + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+        const qeNeg = (-b - Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+        if(a == 0) {
+            return `The variable \'a\' must be greater than 0`
+        }else if (a < 0) {
+            return `(${qePos.toFixed(3)}, 0) (${qeNeg.toFixed(3)}, 0)`
+        }else if (isNaN(qePos) || isNaN(qeNeg)) {
+            return zeros.innerText = `There are no intercepts`;
+        }else {
+            return `(${qeNeg.toFixed(3)}, 0) (${qePos.toFixed(3)}, 0)`
+        }
+    };
+    vertex.innerText = vertexFromula(a, b, c);
+    yInt.innerText = yIntFormula(a, b, c);
+    zeros.innerText = zerosFormula(a, b, c)
 });
-
-
-
-// Reference
-// def yIntFormula(c):
-//     print(f'The y-intercept is: (0, {c})')
-
-// def vertexFormula(a, b, c):
-//     if(a == 0):
-//         print('')
-//     else:
-//         xVert = -b / (2 * a)
-//         yVert = a * (xVert ** 2) + (b * xVert) + c
-//         print(f'The vertex is: ({round(xVert, 3)}, {round(yVert, 3)})')
-
-// def zerosFormula(a, b,c):
-//     if(a == 0):
-//         print('The variable \'a\' must be greater than 0')
-//     elif(a < 0):
-//         qePos = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-//         qeNeg = (-b - math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-//         print(f"The x-intercepts are: ({round(qePos, 3)}, 0) ({round(qeNeg, 3)}, 0)")
-//     else:
-//         qePos = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-//         qeNeg = (-b - math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-//         print(f"The x-intercepts are: ({round(qeNeg, 3)}, 0) ({round(qePos, 3)}, 0)")
-
-// try:
-//     yIntFormula(c)
-//     vertexFormula(a, b, c)
-//     zerosFormula(a, b, c)
-    
-// except:
-//     print("There are no intercepts")
