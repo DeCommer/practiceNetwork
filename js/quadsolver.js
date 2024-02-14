e8003c5a61a236cac99a2f56e01482b22829c4fd
Joseph DeCommer <jdecommer@gmail.com>
2024-02-14 07:20:47 -0500

    sliders now update with given variables

--- js/quadsolver.js
+++ js/quadsolver.js
@@ -1,3 +1,5 @@
+let graph = document.getElementById('calculator');
+let calculator = Desmos.GraphingCalculator(graph);
 
 document.querySelector('.solveBtn').addEventListener('click', function() {
     let a = parseFloat(document.getElementById('a').value);
@@ -10,7 +12,7 @@ document.querySelector('.solveBtn').addEventListener('click', function() {
 
     let vertexFromula = function(a, b, c) {
         if(a == 0) {
-            return `The variable \'a\' must be greater than 0`
+            return `The letiable \'a\' must be greater than 0`
         } else {
             const xVert = -b / (2 * a)
             const yVert = a * (xVert ** 2) + (b * xVert) + c
@@ -19,7 +21,7 @@ document.querySelector('.solveBtn').addEventListener('click', function() {
     };
     let yIntFormula = function(a, b, c) {
         if(a == 0) {
-            return `The variable \'a\' must be greater than 0`
+            return `The letiable \'a\' must be greater than 0`
         } else {
             return `(0, ${c})`
         };
@@ -28,7 +30,7 @@ document.querySelector('.solveBtn').addEventListener('click', function() {
         const qePos = (-b + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
         const qeNeg = (-b - Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
         if(a == 0) {
-            return `The variable \'a\' must be greater than 0`
+            return `The letiable \'a\' must be greater than 0`
         }else if (a < 0) {
             return `(${qePos.toFixed(3)}, 0) (${qeNeg.toFixed(3)}, 0)`
         }else if (isNaN(qePos) || isNaN(qeNeg)) {
@@ -40,4 +42,16 @@ document.querySelector('.solveBtn').addEventListener('click', function() {
     vertex.innerText = vertexFromula(a, b, c);
     yInt.innerText = yIntFormula(a, b, c);
     zeros.innerText = zerosFormula(a, b, c)
+
+    calculator.setExpression({ id: 'q', latex: 'y = ax^2+bx+c' });
+    calculator.setExpression({ id: 'a-slider', latex:  `a = ${a}` });
+    calculator.setExpression({ id: 'b-slider', latex: `b = ${b}` });
+    calculator.setExpression({ id: 'c-slider', latex: `c = ${c}` });
+
 });
+    
+
+
+
+
+
