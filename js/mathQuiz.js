8890dcc372fff10cf2a31502d7e0192e9e6c7ced
Joseph DeCommer <jdecommer@gmail.com>
2024-02-24 13:11:11 -0500

    refactored math quiz

--- js/mathQuiz.js
+++ js/mathQuiz.js
@@ -1,117 +1,104 @@
-var score = 0;
-var questionNum = 1;
-const loopNum = 10;
-const opArr = ["+", "-", "x", "÷"];
-let randOp = opArr[Math.floor(Math.random() * opArr.length)];
-const correctText = document.createElement('div');
-correctText.setAttribute('class', 'correctText');
+let score = 0;
+let questionNum = 1;
+let loopNum = 10;
+let answer;
 
-function correct() {
-    question(); 
-    questionInc();
-    document.getElementById('answerForm').reset();
-}
+let questionNumText = document.getElementById('questionNumber');
+let scoreText = document.getElementById('score');
+questionNumText.textContent = questionNum;
+scoreText.textContent = score;
 
-function question() {
-    leftNum = Math.floor(Math.random() * 10) + 1;
+const messageArea = document.getElementById('messageArea');
+const answerBtn = document.getElementById('AnswerBtn');
+
+let usrIn = document.getElementById('usrIn');
+
+const questionConstructor = () => {
+    opArr = ["+", "-", "x", "÷"];
+    num1 = Math.floor(Math.random() * 10) + 1;
+    num2 = Math.floor(Math.random() * 10) + 1;
     randOp = opArr[Math.floor(Math.random() * opArr.length)];
-    rightNum = Math.floor(Math.random() * 10) + 1;
-    document.getElementById("leftNum").innerHTML = leftNum;
-    document.getElementById("operator").innerHTML = randOp;
-    document.getElementById("rightNum").innerHTML = rightNum;   
-}
-
-function displayCorrect() {
-    correctText.textContent = "Correct";
-    let disp = document.getElementById("correctArea").appendChild(correctText);
-    setTimeout(function(){
-        const element = disp;
-        element.remove();
-    }, 2000);
-}
-
-function displayIncorrect() {
-    correctText.textContent = "Incorrect";
-    let disp = document.getElementById("correctArea").appendChild(correctText);
-    setTimeout(function(){
-        const element = disp;
-        element.remove();
-    }, 2000);
-}
-
-function scoreUpdate() {
-    document.getElementById('score').innerHTML = score;
-}
-
-function questionInc() {
-    questionNum++
-    document.getElementById('questionNumber').innerHTML = questionNum;
-}
-
-document.getElementById('answerForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    guts();
-    if(score >= 25) {
-
+        
+    document.getElementById('leftNum').textContent = num1;
+    document.getElementById('operator').textContent = randOp;
+    document.getElementById('rightNum').textContent = num2;
+    if (randOp === '+') {
+        answer = num1 + num2;
+    }if (randOp === '-') {
+        answer = num1 - num2;
+    }if (randOp === 'x') {
+        answer = num1 * num2;
+    }if (randOp === '÷') {
+        answer = num1 / num2;
+        answer = answer.toFixed(2);
     }
+    return answer
+}
+
+const correctCheck = () => {
+    if(randOp === '+' && Number(usrIn.value) === Number(answer)) {
+        // console.log("This was a plus question")
+        questionRight();
+        score += 1;
+        scoreText.textContent = score;
+        // console.log(`The score is: ${score}`);
+    }else if(randOp ==='-' && Number(usrIn.value) === Number(answer)) {
+        // console.log("This was a Minus question")
+        questionRight();
+        score += 2;
+        scoreText.textContent = score;
+    }else if(randOp === 'x' && Number(usrIn.value) === Number(answer)) {
+        // console.log("This was a multiplication question")
+        questionRight();
+        score += 5;
+        scoreText.textContent = score;
+    }else if(randOp === '÷' && Number(usrIn.value) === Number(answer)) {
+        // console.log("This was a Division question")
+        questionRight();
+        score += 10;
+        scoreText.textContent = score;
+    } else if (Number(usrIn.value) !== Number(answer)) {
+        questionWrong();
+        score -= 1;
+        scoreText.textContent = score;
+    }
+}
+
+const clearField = () => {
+    usrIn.value = null;
+    usrIn.focus();
+}
+
+const messageTimeout = () => {
+    setTimeout(() =>{
+        messageArea.textContent = '';
+    }, 1500);
+}
+const questionRight = () => {
+    clearField();
+    messageArea.textContent = 'Correct!'
+    messageTimeout();
+}
+
+const questionWrong = () => {
+    clearField();
+    messageArea.textContent = 'Incorrect!'
+    messageTimeout();
+}
+
+const nextQuestion = () => {
+    questionNum += 1
+    questionNumText.textContent = questionNum;
+    questionConstructor();
+    // console.log(answer);
+}
+
+answerBtn.addEventListener('click', () => {
+    correctCheck();
+    nextQuestion();
+    // console.log(`${randOp}: ${typeof(randOp)}`)
 });
 
-function guts() {
-    
-    let usrAnswer = parseFloat(document.getElementById('answerField').value);
-
-    if(randOp === "+") {
-        answer = (leftNum + rightNum);
-        if(usrAnswer == answer) {
-            correct();
-            displayCorrect();
-            score += 1;
-            scoreUpdate();
-        }else if(usrAnswer != answer) {
-            displayIncorrect()
-            score -= 1;
-            scoreUpdate();
-        }
-    }
-    if(randOp === "-") {
-        answer = (leftNum - rightNum);
-        if(usrAnswer == answer) {
-            correct();
-            displayCorrect();
-            score += 2;
-            scoreUpdate();
-        }else if(usrAnswer != answer) {
-            displayIncorrect()
-            score -= 1;
-            scoreUpdate();
-        }
-    }
-    if(randOp === "x") {
-        answer = (leftNum * rightNum);
-        if(usrAnswer == answer) {
-            correct();
-            displayCorrect();
-            score += 5;
-            scoreUpdate();
-        }else if(usrAnswer != answer) {
-            displayIncorrect()
-            score -= 1;
-            scoreUpdate();
-        }
-    }
-    if(randOp === "÷") {
-        answer = (leftNum / rightNum).toFixed(2);
-        if(usrAnswer == answer) {
-            correct();
-            displayCorrect();
-            score += 10;
-            scoreUpdate();
-        }else if(usrAnswer != answer) {
-            displayIncorrect()
-            score -= 1;
-            scoreUpdate();
-        }
-    }
-}
-
-question();
+questionConstructor();
+// console.log(answer);
+// console.log(`${randOp}: ${typeof(randOp)}`);
