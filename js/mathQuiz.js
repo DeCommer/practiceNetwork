520d3914f198336eef61e01e887886aacef23665
Joseph DeCommer <jdecommer@gmail.com>
2024-02-24 13:15:09 -0500

    added new code to modal

--- js/mathQuiz.js
+++ js/mathQuiz.js
@@ -2,7 +2,7 @@ let score = 0;
 let questionNum = 1;
 let loopNum = 10;
 let answer;
-
+let usrIn = document.getElementById('usrIn');
 let questionNumText = document.getElementById('questionNumber');
 let scoreText = document.getElementById('score');
 questionNumText.textContent = questionNum;
@@ -10,8 +10,6 @@ scoreText.textContent = score;
 
 const messageArea = document.getElementById('messageArea');
 const answerBtn = document.getElementById('AnswerBtn');
-
-let usrIn = document.getElementById('usrIn');
 
 const questionConstructor = () => {
     opArr = ["+", "-", "x", "÷"];
@@ -102,3 +100,99 @@ answerBtn.addEventListener('click', () => {
 questionConstructor();
 // console.log(answer);
 // console.log(`${randOp}: ${typeof(randOp)}`);
+
+//Old==================================================================
+
+// var score = 0;
+// var questionNum = 1;
+// const loopNum = 10;
+// const opArr = ["+", "-", "x", "÷"];
+// let randOp = opArr[Math.floor(Math.random() * opArr.length)];
+// const correctText = document.createElement('div');
+// correctText.setAttribute('class', 'correctText');
+
+// function correct() {
+//     question(); 
+//     questionInc();
+//     document.getElementById('answerForm').reset();
+// }
+
+// function question() {
+//     leftNum = Math.floor(Math.random() * 10) + 1;
+//     randOp = opArr[Math.floor(Math.random() * opArr.length)];
+//     rightNum = Math.floor(Math.random() * 10) + 1;
+//     document.getElementById("leftNum").innerHTML = leftNum;
+//     document.getElementById("operator").innerHTML = randOp;
+//     document.getElementById("rightNum").innerHTML = rightNum;   
+// }
+
+// function displayCorrect() {
+//     correctText.textContent = "Correct";
+//     let disp = document.getElementById("correctArea").appendChild(correctText);
+//     setTimeout(function(){
+//         const element = disp;
+//         element.remove();
+//     }, 2000);
+// }
+
+// function scoreUpdate() {
+//     document.getElementById('score').innerHTML = score;
+// }
+
+// function questionInc() {
+//     questionNum++
+//     document.getElementById('questionNumber').innerHTML = questionNum;
+// }
+
+// document.getElementById('answerForm').addEventListener('submit', function(e) {
+//     e.preventDefault();
+//     guts();
+//     if(score >= 25) {
+
+//     }
+// });
+
+// function guts() {
+    
+//     let usrAnswer = parseFloat(document.getElementById('answerField').value);
+
+//     if(randOp === "+") {
+//         answer = (leftNum + rightNum);
+//         if(usrAnswer == answer) {
+//             correct();
+//             displayCorrect();
+//             score += 1;
+//             scoreUpdate();
+//         }
+//     }
+//     if(randOp === "-") {
+//         answer = (leftNum - rightNum);
+//         if(usrAnswer == answer) {
+//             correct();
+//             displayCorrect();
+//             score += 2;
+//             scoreUpdate();
+//         }
+//     }
+//     if(randOp === "x") {
+//         answer = (leftNum * rightNum);
+//         if(usrAnswer == answer) {
+//             correct();
+//             displayCorrect();
+//             score += 5;
+//             scoreUpdate();
+//         }
+//     }
+//     if(randOp === "÷") {
+//         answer = (leftNum / rightNum).toFixed(2);
+//         console.log(answer);
+//         if(usrAnswer == answer) {
+//             correct();
+//             displayCorrect();
+//             score += 10;
+//             scoreUpdate();
+//         }
+//     }
+// }
+
+// question();
