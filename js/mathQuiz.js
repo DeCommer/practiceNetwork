ef2eb9ef1b5d846524ffc36a64a843f27bb91f56
Joseph DeCommer <jdecommer@gmail.com>
2024-02-28 11:13:37 -0500

    code clean up

--- js/mathQuiz.js
+++ js/mathQuiz.js
@@ -5,8 +5,6 @@ let correct = 0;
 let incorrect = 0;
 let answer;
 let usrIn = document.getElementById('usrIn');
-// let questionNumText = document.getElementById('questionNumber');
-// questionNumText.textContent = questionNum;
 let scoreText = document.getElementById('score');
 
 const bgColorChange = document.querySelector('.converter-container');
@@ -37,6 +35,7 @@ startTimeBtn.addEventListener('click', () => {
         clearInterval(int);
     }
     int = setInterval(displayTimer, 10)
+    usrIn.focus();
 })
 
 pauseTimeBtn.addEventListener('click', () => {
@@ -68,7 +67,7 @@ const displayTimer = () => {
             hours++;
         }
     }
-    let h = hours < 10 ? '0' + hours : hours;
+    // let h = hours < 10 ? '0' + hours : hours;
     let m = minutes < 10 ? '0' + minutes : minutes;
     let s = seconds < 10 ? '0' + seconds : seconds;
     let ms = milliseconds < 10 ? '00' + milliseconds : milliseconds < 100 ? '0' + milliseconds : milliseconds
@@ -78,7 +77,6 @@ const displayTimer = () => {
 }
 
 // Main Program ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
 const questionConstructor = () => {
     opArr = ["+", "-", "x", "÷"];
     num1 = Math.floor(Math.random() * 10) + 1;
@@ -88,7 +86,6 @@ const questionConstructor = () => {
     document.getElementById('operator').textContent = randOp;
     document.getElementById('rightNum').textContent = num2;
 }
-
 const operatorModes = () => {
     if(allOpsBtn.classList[1] === 'active') {
         allOps();
@@ -188,7 +185,6 @@ const messageTimeout = () => {
 
 const questionRight = () => {
     correct += 1
-    // console.log(`Correct: ${correct}`);
     bgColorChange.style.boxShadow = '0 0 50px 20px rgba(51, 253, 0, 0.575)';
     setTimeout(() =>{
         bgColorChange.style.boxShadow = '0 0 0 0';
@@ -201,13 +197,11 @@ const questionRight = () => {
 
 const questionWrong = () => {
     incorrect += 1
-    // console.log(`Incorrect: ${incorrect}`);
     bgColorChange.style.boxShadow = '0 0 50px 20px rgb(255, 46, 46)';
     setTimeout(() =>{
         bgColorChange.style.boxShadow = '0 0 0 0';
     }, 1800);
     messageArea.style.color = '#c1121f';
-    // messageArea.textContent = 'Incorrect!'
     messageArea.textContent = `The correct answer is: ${answer}`;
     clearField();
     messageTimeout();
@@ -215,23 +209,18 @@ const questionWrong = () => {
 
 const correctCheck = () => {
     if(randOp === '+' && Number(usrIn.value) === Number(answer)) {
-        // console.log("This was a plus question")
         questionRight();
         score += 1;
         scoreText.textContent = score;
-        // console.log(`The score is: ${score}`);
     }else if(randOp ==='-' && Number(usrIn.value) === Number(answer)) {
-        // console.log("This was a Minus question")
         questionRight();
         score += 2;
         scoreText.textContent = score;
     }else if(randOp === 'x' && Number(usrIn.value) === Number(answer)) {
-        // console.log("This was a multiplication question")
         questionRight();
         score += 5;
         scoreText.textContent = score;
     }else if(randOp === '÷' && Number(usrIn.value) === Number(answer)) {
-        // console.log("This was a Division question")
         questionRight();
         score += 10;
         scoreText.textContent = score;
@@ -260,17 +249,11 @@ const hasWon = () => {
         timeStat.innerHTML = displayTimer();
     }
     clearTime();
-    // console.log(`Total Questions: ${questionNum}`);
-    // console.log(`correct: ${correct}`);
-    // console.log(`Incorrect: ${incorrect}`);
-    // console.log(`Percent correct:${percentCorrect.toFixed(2)}%`);
 }
 
 const nextQuestion = () => {
     questionNum += 1
-    // questionNumText.textContent = questionNum;
     operatorModes();
-    // console.log(answer);
 }
 
 const reset = () => {
@@ -279,55 +262,22 @@ const reset = () => {
     correct = 0;
     incorrect = 0;
     progressBar();
-    // questionNumText.textContent = 0;
     scoreText.textContent = score;
 }
 
 const progressBar = () => {
     let increment = 100 / numberOfQuestions;
-
     let progress = document.querySelector('.progress-bar');
     let width = progress.style.width.replace('%', '');
-
     width = parseInt(width) + increment;
-
     width = (questionNum / numberOfQuestions) * 100+'%';
     progress.style.width = width;
-
-    // console.log(`QuestionNum: ${questionNum}`)
-    
-    // if (width >= 100) {
-    //     progress.style.width = '100%';
-    // }if (questionNum == 0) {
-    //     width = 0+'%';
-    //     progress.style.width = width;
-    // }
-    // if (questionNum == 1) {
-    //     width = (1 / numberOfQuestions) * 100+'%';
-    //     progress.style.width = width;
-    // }
-    // if (questionNum == 2) {
-    //     width = (2 / numberOfQuestions) * 100+'%';
-    //     progress.style.width = width;
-    // }if (questionNum == 3) {
-    //     width = (3 / numberOfQuestions) * 100+'%';
-    //     progress.style.width = width;
-    // }
-    // if (questionNum == 4) {
-    //     width = (4 / numberOfQuestions) * 100+'%';
-    //     progress.style.width = width;
-    // }
-
-
-    // console.log(`${numberOfQuestions}`)
-    // console.log(`${(questionNum / numberOfQuestions) * 100}`)
 }
 progressBar()
 
 answerBtn.addEventListener('click', () => {
     correctCheck();
     nextQuestion();
-    // console.log(`${randOp}: ${typeof(randOp)}`)
     if(questionNum === numberOfQuestions) {
         hasWon();
     }
@@ -339,18 +289,14 @@ usrIn.addEventListener('keydown', (event) => {
         correctCheck();
         nextQuestion();
     }
-
     if(questionNum === numberOfQuestions) {
         hasWon();
     }
     progressBar()
 });
 operatorModes();
-// console.log(answer);
-// console.log(`${randOp}: ${typeof(randOp)}`);
 
-// Win modal
-
+// Win modal ++++++++++++++++++++++++++++++++++++++++++++++++
 const winModal = document.querySelector('.winModal');
 const winOverlay = document.querySelector('.winOverlay');
 
@@ -363,104 +309,6 @@ const closeWinModal = () => {
     winModal.classList.add('hidden');
     winOverlay.classList.add('hidden');
     reset();
-    // console.log(questionNum)
 }
-
 document.querySelector('.closeWinModal').addEventListener('click', closeWinModal);
 document.querySelector('.winOverlay').addEventListener('click', closeWinModal);
-
-//Old==================================================================
-
-// var score = 0;
-// var questionNum = 1;
-// const loopNum = 10;
-// const opArr = ["+", "-", "x", "÷"];
-// let randOp = opArr[Math.floor(Math.random() * opArr.length)];
-// const correctText = document.createElement('div');
-// correctText.setAttribute('class', 'correctText');
-
-// function correct() {
-//     question(); 
-//     questionInc();
-//     document.getElementById('answerForm').reset();
-// }
-
-// function question() {
-//     leftNum = Math.floor(Math.random() * 10) + 1;
-//     randOp = opArr[Math.floor(Math.random() * opArr.length)];
-//     rightNum = Math.floor(Math.random() * 10) + 1;
-//     document.getElementById("leftNum").innerHTML = leftNum;
-//     document.getElementById("operator").innerHTML = randOp;
-//     document.getElementById("rightNum").innerHTML = rightNum;   
-// }
-
-// function displayCorrect() {
-//     correctText.textContent = "Correct";
-//     let disp = document.getElementById("correctArea").appendChild(correctText);
-//     setTimeout(function(){
-//         const element = disp;
-//         element.remove();
-//     }, 2000);
-// }
-
-// function scoreUpdate() {
-//     document.getElementById('score').innerHTML = score;
-// }
-
-// function questionInc() {
-//     questionNum++
-//     document.getElementById('questionNumber').innerHTML = questionNum;
-// }
-
-// document.getElementById('answerForm').addEventListener('submit', function(e) {
-//     e.preventDefault();
-//     guts();
-//     if(score >= 25) {
-
-//     }
-// });
-
-// function guts() {
-    
-//     let usrAnswer = parseFloat(document.getElementById('answerField').value);
-
-//     if(randOp === "+") {
-//         answer = (leftNum + rightNum);
-//         if(usrAnswer == answer) {
-//             correct();
-//             displayCorrect();
-//             score += 1;
-//             scoreUpdate();
-//         }
-//     }
-//     if(randOp === "-") {
-//         answer = (leftNum - rightNum);
-//         if(usrAnswer == answer) {
-//             correct();
-//             displayCorrect();
-//             score += 2;
-//             scoreUpdate();
-//         }
-//     }
-//     if(randOp === "x") {
-//         answer = (leftNum * rightNum);
-//         if(usrAnswer == answer) {
-//             correct();
-//             displayCorrect();
-//             score += 5;
-//             scoreUpdate();
-//         }
-//     }
-//     if(randOp === "÷") {
-//         answer = (leftNum / rightNum).toFixed(2);
-//         console.log(answer);
-//         if(usrAnswer == answer) {
-//             correct();
-//             displayCorrect();
-//             score += 10;
-//             scoreUpdate();
-//         }
-//     }
-// }
-
-// question();
