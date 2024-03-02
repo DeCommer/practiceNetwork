f6b349ad330c531b33293cfab234b038ef749fd9
Joseph DeCommer <jdecommer@gmail.com>
2024-03-02 18:11:24 -0500

    automated year change

--- js/main.js
+++ js/main.js
@@ -1,24 +1,6 @@
-// const sections = document.querySelectorAll('.section');
-
-// window.addEventListener('scroll', checkSection)
-
-// checkSection();
-
-// function checkSection() {
-//     const triggerTop = window.innerHeight / 4;
-    
-
-//     sections.forEach(section => {
-//         const sectionTop = section.getBoundingClientRect().top;
-
-//         if(sectionTop < triggerTop) {
-//             section.classList.remove('show');
-//         }else {
-//             section.classList.add('show');
-//         }
-//     })
-// }
-
+const year = document.querySelector('.year');
+getYear = new Date().getFullYear();
+year.innerText = getYear;
 
 
 
