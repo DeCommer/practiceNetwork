554023d2575664930094c91c20e1def09d9a97cb
Joseph DeCommer <jdecommer@gmail.com>
2024-04-29 11:17:53 -0400

    Added 3 percentage problem types solver

--- js/percentcalc.js
+++ js/percentcalc.js
@@ -109,42 +109,86 @@ chngResetBtn.addEventListener('click', () =>{
 //Common Phrases Calculator
 const type_1_1 = document.getElementById('type-1-1');
 const type_1_2 = document.getElementById('type-1-2');
-const type_2_1 = document.getElementById('type-2-1');
-const type_2_2 = document.getElementById('type-2-2');
-const type_3_1 = document.getElementById('type-3-1');
-const type_3_2 = document.getElementById('type-3-2');
 const type1Btn = document.getElementById('type-1-btn');
 const type2Btn = document.getElementById('type-2-btn');
 const type3Btn = document.getElementById('type-3-btn');
 const typeEnterBtn = document.getElementById('type-enter-btn');
 const typeResetBtn = document.getElementById('type-reset-btn');
 const typeMessage = document.getElementById('type-message');
-const typeResult = document.getElementById('ype-result');
+const typeResult = document.getElementById('type-result');
 const typeFormula = document.getElementById('type-formula');
 const inputQuestionTxt = document.getElementById('input-question-type');
 
 let type = 1;
 
 type1Btn.addEventListener('click', () => {
+    type = 1;
+    inputQuestionTxt.innerHTML = 'What is <input id="type-1-1" type="number" autocomplete="off">% of <input id="type-1-2" type="number" autocomplete="off">?';
+    console.log(`Type: ${type}`);
     type1Btn.classList.add('active');
     type2Btn.classList.remove('active');
     type3Btn.classList.remove('active');
-    inputQuestionTxt.innerHTML = 'What is <input id="type-1-1" type="number" autocomplete="off">% of <input id="type-1-2" type="number" autocomplete="off">'
+    typeMessage.textContent = '';
+    typeResult.textContent = '0';
+    typeFormula.textContent = '';
 });
 
 type2Btn.addEventListener('click', () => {
+    type = 2;
+    inputQuestionTxt.innerHTML = '<input id="type-2-1" type="number" autocomplete="off"> is what % of <input id="type-2-2" type="number" autocomplete="off">?';
+    console.log(`Type: ${type}`);
     type1Btn.classList.remove('active');
     type2Btn.classList.add('active');
     type3Btn.classList.remove('active');
-    inputQuestionTxt.innerHTML = '<input id="type-2-1" type="number" autocomplete="off"> is what % of <input id="type-2-2" type="number" autocomplete="off">'
+    typeMessage.textContent = '';
+    typeResult.textContent = '0';
+    typeFormula.textContent = '';
 });
 
 type3Btn.addEventListener('click', () => {
+    type = 3;
+    inputQuestionTxt.innerHTML = '<input id="type-3-1" type="number" autocomplete="off">is<input id="type-3-2" type="number" autocomplete="off">% of what ?';
+    console.log(`Type: ${type}`);
     type1Btn.classList.remove('active');
     type2Btn.classList.remove('active');
     type3Btn.classList.add('active');
-    inputQuestionTxt.innerHTML = '<input id="type-3-1" type="number" autocomplete="off">is<input id="type-3-2" type="number" autocomplete="off">% of what'
+    typeMessage.textContent = '';
+    typeResult.textContent = '0';
+    typeFormula.textContent = '';
 });
 
+typeEnterBtn.addEventListener('click', () => {
+    if(type === 1){
+        const type_1_1 = document.getElementById('type-1-1');
+        const type_1_2 = document.getElementById('type-1-2');
+        const type1Out = Number((type_1_1.value / 100) * Number(type_1_2.value)).toFixed(2);
+        typeResult.innerHTML = `${type1Out}<span> is ${type_1_1.value}% of ${type_1_2.value}</span>`;
+        typeFormula.textContent = 'Formula: (Percent / 100) x Value';
+    }else if(type === 2) {
+        const type_2_1 = document.getElementById('type-2-1');
+        const type_2_2 = document.getElementById('type-2-2');
+        const type2Out = (Number(type_2_1.value) / Number(type_2_2.value));
+        typeResult.innerHTML = `<span>${type_2_1.value} is</span> ${(type2Out * 100).toFixed(4)}% <span>of ${type_2_2.value}</span>`;
+        typeFormula.textContent = 'Formula: (Value 1 / Value 2) * 100';
+    }else if(type === 3) {
+        const type_3_1 = document.getElementById('type-3-1');
+        const type_3_2 = document.getElementById('type-3-2');
+        const type3Out = (Number(type_3_1.value) / (Number(type_3_2.value) / 100));
+        typeResult.innerHTML = `<span>${type_3_1.value} is ${type_3_2.value}% of</span> ${(type3Out).toFixed(4)}`;
+        typeFormula.textContent = 'Formula: (Value 1 / (Value 2 / 100))';
+    }
 
+});
+
+typeResetBtn.addEventListener('click', () => {
+    type1Btn.classList.add('active');
+    type2Btn.classList.remove('active');
+    type3Btn.classList.remove('active');
+    type_1_1.value = '';
+    type_1_2.value = '';
+    typeMessage.textContent = '';
+    typeResult.textContent = '0';
+    typeFormula.textContent = '';
+    inputQuestionTxt.innerHTML = 'What is <input id="type-1-1" type="number" autocomplete="off">% of <input id="type-1-2" type="number" autocomplete="off">?';
+});
 
