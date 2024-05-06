965c64211d922814468a264464a8927002762e35
Joseph DeCommer <jdecommer@gmail.com>
2024-05-06 08:45:47 -0400

    BMR app (Wip)

---
+++ js/bmrcalc.js
@@ -0,0 +1,52 @@
+const usBtn = document.getElementById('us-btn');
+const metricBtn = document.getElementById('metric-btn');
+const maleBtn = document.getElementById('m-btn');
+const femaleBtn = document.getElementById('f-btn');
+const ftIn = document.getElementById('feet-input');
+const inIn = document.getElementById('inch-input');
+const ageIn = document.getElementById('age-input');
+const poundsIn = document.getElementById('pounds-input');
+const metricContainer = document.getElementById('metric');
+const cmIn = document.getElementById('cm-input');
+const metAgeIn = document.getElementById('met-age-input');
+const kgIn = document.getElementById('kg-input');
+const calcBtn = document.getElementById('calc-btn');
+const resetBtn = document.getElementById('reset-btn');
+const messageTxt = document.getElementById('message-txt');
+const resultsTxt = document.getElementById('results-txt');
+const formulaTxt = document.getElementById('formula-txt');
+
+let unit = 'us';
+let gender = 'm';
+
+usBtn.addEventListener('click', () => {
+    unit = 'us';
+    usBtn.classList.add('active');
+    metricBtn.classList.remove('active');
+});
+
+metricBtn.addEventListener('click', () => {
+    unit = 'metric';
+    usBtn.classList.remove('active');
+    metricBtn.classList.add('active');
+});
+
+maleBtn.addEventListener('click', () => {
+    gender = 'm';
+    maleBtn.classList.add('active');
+    femaleBtn.classList.remove('active');
+});
+
+femaleBtn.addEventListener('click', () => {
+    gender = 'f';
+    maleBtn.classList.remove('active');
+    femaleBtn.classList.add('active');
+});
+
+calcBtn.addEventListener('click', () => {
+
+});
+
+resetBtn.addEventListener('click', () => {
+
+});
