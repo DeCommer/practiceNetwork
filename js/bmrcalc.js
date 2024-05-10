d26293427851d31719b321365da489e0e90b5876
Joseph DeCommer <jdecommer@gmail.com>
2024-05-10 12:47:52 +0000

    refactored, added enhanced info text

--- js/bmrcalc.js
+++ js/bmrcalc.js
@@ -21,52 +21,12 @@ const formulaTxt = document.getElementById('formula-txt');
 let unit = 'us';
 let gender = 'm';
 
-usBtn.addEventListener('click', () => {
-    unit = 'us';
-    usBtn.classList.add('active');
-    metricBtn.classList.remove('active');
-    usIn.classList.remove('hide');
-    metIn.classList.add('hide');
-    messageTxt.textContent = `Enter info above`
-    resultsTxt.innerHTML = `<p id="results-txt">0<span>calories/day</span></p>`;
-    formulaTxt.textContent = ``
-});
-
-metricBtn.addEventListener('click', () => {
-    unit = 'metric';
-    usBtn.classList.remove('active');
-    metricBtn.classList.add('active');
-    usIn.classList.add('hide');
-    metIn.classList.remove('hide');
-    messageTxt.textContent = `Enter info above`
-    resultsTxt.innerHTML = `<p id="results-txt">0<span>calories/day</span></p>`;
-    formulaTxt.textContent = ``
-});
-
-maleBtn.addEventListener('click', () => {
-    gender = 'm';
-    maleBtn.classList.add('active');
-    femaleBtn.classList.remove('active');
-    messageTxt.textContent = `Enter info above`
-    resultsTxt.innerHTML = `<p id="results-txt">0<span>calories/day</span></p>`;
-    formulaTxt.textContent = ``
-});
-
-femaleBtn.addEventListener('click', () => {
-    gender = 'f';
-    maleBtn.classList.remove('active');
-    femaleBtn.classList.add('active');
-    messageTxt.textContent = `Enter info above`
-    resultsTxt.innerHTML = `<p id="results-txt">0<span>calories/day</span></p>`;
-    formulaTxt.textContent = ``
-});
-
 const usCalc = () => {
-    const h = ((Number(ftIn.value) * 12) + Number(inIn.value) * 2.54); //converts to cm
-    const w = poundsIn.value * 0.45359237; //converts to kg
+    const h = ((Number(ftIn.value) * 12) + Number(inIn.value) * 2.54); //converts to cm 82.86
+    const w = poundsIn.value * 0.45359237; //converts to kg 86.1826
     const a = ageIn.value;
-    const bmr = 10 * w + 6.25 * h - 5 * a;
-    return bmr.toFixed(0);
+    const bmr = 10 * w + 6.25 * h - 5 * a;  //should = 1159.7
+    return bmr;
 };
 
 const metricBmr = () => {
@@ -74,35 +34,10 @@ const metricBmr = () => {
     const w = kgIn.value
     const a = metAgeIn.value;
     const metricBmr = 10 * w + 6.25 * h - 5 * a;
-    return metricBmr.toFixed(0);
+    return metricBmr;
 };
 
-calcBtn.addEventListener('click', () => {
-    //BMR = 10W + 6.25H - 5A + 5 (Men)
-    //BMR = 10W + 6.25H - 5A - 161 (Women)
-
-    if(unit === 'us' && gender === 'm') {
-        messageTxt.textContent = `Your BMR is:`
-        resultsTxt.innerHTML = `<p id="results-txt">${Number(usCalc() + Number(5))}<span>calories/day</span></p>`;
-        formulaTxt.textContent = `10W + 6.25H - 5A + 5`
-    }else if (unit == 'us' && gender === 'f') {
-        messageTxt.textContent = `Your BMR is:`
-        resultsTxt.innerHTML = `<p id="results-txt">${Number(usCalc() - Number(161))}<span>calories/day</span></p>`;
-        formulaTxt.textContent = `10W + 6.25H - 5A - 161`
-    }else if (unit === 'metric' && gender === 'm') {
-        messageTxt.textContent = `Your BMR is:`
-        resultsTxt.innerHTML = `<p id="results-txt">${Number(metricBmr() + Number(5))}<span>calories/day</span></p>`;
-        formulaTxt.textContent = `10W + 6.25H - 5A + 5`
-    }else if (unit === 'metric' && gender === 'f') {
-        messageTxt.textContent = `Your BMR is:`
-        resultsTxt.innerHTML = `<p id="results-txt">${Number(metricBmr() - Number(161))}<span>calories/day</span></p>`;
-        formulaTxt.textContent = `10W + 6.25H - 5A - 161`
-    };
-});
-
-resetBtn.addEventListener('click', () => {
-    unit = 'us';
-    gender ='m';
+const resetInput = () => {
     inIn.value = '';
     ftIn.value = '';
     poundsIn.value = '';
@@ -114,4 +49,64 @@ resetBtn.addEventListener('click', () => {
     messageTxt.textContent = `Enter info above`
     resultsTxt.innerHTML = `<p id="results-txt">0</p>`;
     formulaTxt.textContent = ``;
+};
+
+usBtn.addEventListener('click', () => {
+    unit = 'us';
+    usBtn.classList.add('active');
+    metricBtn.classList.remove('active');
+    usIn.classList.remove('hide');
+    metIn.classList.add('hide');
+    resetInput();
+});
+
+metricBtn.addEventListener('click', () => {
+    unit = 'metric';
+    usBtn.classList.remove('active');
+    metricBtn.classList.add('active');
+    usIn.classList.add('hide');
+    metIn.classList.remove('hide');
+    resetInput();
+});
+
+maleBtn.addEventListener('click', () => {
+    gender = 'm';
+    maleBtn.classList.add('active');
+    femaleBtn.classList.remove('active');
+    resetInput();
+});
+
+femaleBtn.addEventListener('click', () => {
+    gender = 'f';
+    maleBtn.classList.remove('active');
+    femaleBtn.classList.add('active');
+    resetInput();
+});
+
+calcBtn.addEventListener('click', () => {
+    //BMR = 10W + 6.25H - 5A + 5 (Men)
+    //BMR = 10W + 6.25H - 5A - 161 (Women)
+    if(unit === 'us' && gender === 'm') {
+        messageTxt.textContent = `Your BMR is:`
+        resultsTxt.innerHTML = `<p id="results-txt">${Number(usCalc() + Number(5))}<span>calories/day</span></p>`;
+        formulaTxt.textContent = `BMR = 10W + 6.25H - 5A + 5`
+    }else if (unit == 'us' && gender === 'f') {
+        messageTxt.textContent = `Your BMR is:`
+        resultsTxt.innerHTML = `<p id="results-txt">${Number(usCalc() - Number(161))}<span>calories/day</span></p>`;
+        formulaTxt.textContent = `BMR = 10W + 6.25H - 5A - 161`
+    }else if (unit === 'metric' && gender === 'm') {
+        messageTxt.textContent = `Your BMR is:`
+        resultsTxt.innerHTML = `<p id="results-txt">${Number(metricBmr() + Number(5))}<span>calories/day</span></p>`;
+        formulaTxt.textContent = `BMR = 10W + 6.25H - 5A + 5`
+    }else if (unit === 'metric' && gender === 'f') {
+        messageTxt.textContent = `Your BMR is:`
+        resultsTxt.innerHTML = `<p id="results-txt">${Number(metricBmr() - Number(161))}<span>calories/day</span></p>`;
+        formulaTxt.textContent = `BMR = 10W + 6.25H - 5A - 161`
+    };
+});
+
+resetBtn.addEventListener('click', () => {
+    unit = 'us';
+    gender ='m';
+    resetInput();
 });
