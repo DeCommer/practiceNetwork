a653fb9aa207bc0a551ae9f5a71024b488c4b512
Joseph DeCommer <jdecommer@gmail.com>
2024-05-16 06:35:34 -0400

    Cleaned up convert function (wip)

--- superconverter/superconverter.js
+++ superconverter/superconverter.js
@@ -1,15 +1,27 @@
+const typeSelector = document.getElementById('type-selector');
 const input = document.getElementById('input');
 const from = document.getElementById('from');
 const to = document.getElementById('to');
 const result = document.getElementById('result');
 const swapBtn = document.getElementById('swap-btn');
+const formula = document.getElementById('formula');
 
 const convert = () => {
-    const convertedValue = (input.value) * 1000000;
-    result.innerHTML = `<div class="result" id="result"><p>${convertedValue}</p><span class="result-span">Sq Meter</span></div>`;
+    if(typeSelector.value === 'area') {
+        if(from.value === 'sqk' && to.value === 'sqk') {
+            const kmToKm = (input.value) * 1;
+            result.innerHTML = `<p>${kmToKm}</p><span class="result-span">Square Kilometer</span>`;
+            formula.innerHTML = `<p><span>Formula</span>multiply the area value by 1</p>`
+        }else if(from.value === 'sqk' && to.value === 'sqm') {
+            const kmToM = (input.value) * 1000000;
+            result.innerHTML = `<p>${kmToM}</p><span class="result-span">Square Meters</span>`;
+            formula.innerHTML = `<p><span>Formula</span>multiply the area value by 1000000</p>`
+        }else {
+            result.innerHTML = `<p>0</p><span class="result-span">Enter Value</span>`;
+            formula.innerHTML = `<p><span>Formula</span></p>`
+        }
+    }
 };
-
-
 
 to.addEventListener('change', convert);
 from.addEventListener('change', convert);
