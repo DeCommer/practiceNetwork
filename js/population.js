2707758721d536fbdbb796494982db389715dc9b
Joseph DeCommer <jdecommer@gmail.com>
2024-05-21 08:02:59 -0400

    Added world population counter

---
+++ js/population.js
@@ -0,0 +1,28 @@
+// const worldPopTxt = document.getElementById('world-pop-txt');
+// const usPopTxt = document.getElementById('us-pop-txt');
+
+// let startWorldPop = 8048729900;
+
+// const startPop = () => {
+//     startWorldPop++;
+//     worldPopTxt.innerHTML =  `${startWorldPop}`;
+// }
+// setInterval(startPop, 1500);
+
+let population = 8110872986; 
+const growthRatePerSecond = 2.4;
+const worldPopTxt = document.getElementById('world-pop-txt');
+
+function formatNumber(num) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function updatePopulation() {
+    const secondsSinceLoad = (new Date().getTime() - startTime) / 1000;
+    const currentPopulation = population + (secondsSinceLoad * growthRatePerSecond);
+    worldPopTxt.textContent = formatNumber(Math.floor(currentPopulation));
+}
+
+const startTime = new Date().getTime();
+updatePopulation();
+setInterval(updatePopulation, 1000);
