c5c7a6db7cd8403e4f2db4f9ebd79db714eb6f58
Joseph DeCommer <jdecommer@gmail.com>
2024-05-22 07:01:07 -0400

    population update

--- js/population.js
+++ js/population.js
@@ -9,7 +9,7 @@
 // }
 // setInterval(startPop, 1500);
 
-let population = 8110872986; 
+let population = 8049110500; 
 const growthRatePerSecond = 2.4;
 const worldPopTxt = document.getElementById('world-pop-txt');
 
