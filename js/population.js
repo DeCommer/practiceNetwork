6ca8a303d91ec08569a79d49027dfcad4c5bacc6
Joseph DeCommer <jdecommer@gmail.com>
2024-05-31 06:44:16 -0400

    population update, map modal added

--- js/population.js
+++ js/population.js
@@ -9,7 +9,7 @@
 // }
 // setInterval(startPop, 1500);
 
-let population = 8_050_934_892; 
+let population = 8_051_934_892; 
 const growthRatePerSecond = 2.4;
 const worldPopTxt = document.getElementById('world-pop-txt');
 
--- map/js/map.js
+++ map/js/map.js
@@ -1,8 +1,15 @@
 const states = document.querySelectorAll('.state');
 const stateName = document.getElementById('state-name');
+const display = document.getElementById('display');
+const closeModal = document.getElementById('closeModal')
 
-states.forEach((state, index) => {
+states.forEach((state) => {
     state.addEventListener('click', () =>{
         stateName.textContent = `${state.dataset.name}`;
+        display.classList.remove('hidden');
     });
+});
+
+closeModal.addEventListener('click', () => {
+    display.classList.add('hidden');
 });
