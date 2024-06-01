0517e0fba04c76d5ef1f2e5ee962b1255bf59eb8
Joseph DeCommer <jdecommer@gmail.com>
2024-06-01 15:00:29 -0400

    Added number formatting function

--- map/js/map.js
+++ map/js/map.js
@@ -7,6 +7,10 @@ fetch("./us_states.json")
 .then(response => response.json())
 .then(data => displayStates(data));
 
+function formatNumber(num) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 const displayStates = (data) => {
     // const tableMain = document.querySelector('.table-main');
     const closeModal = document.querySelector('.closeModal');
@@ -21,18 +25,20 @@ const displayStates = (data) => {
             console.log(data.elements[i].name);
             const dataDisplay = document.querySelector('.data');
             let html =`
-                <h1>${data.elements[i].name}</h1>
-                <div>
+            <div class="data-container">
+                <h1 class="title">${data.elements[i].name}</h1>
+                <div class="info-container">
                     <ul>
                         <li><span>Capital: </span>${data.elements[i].capital}</li>
                         <li><span>Largest City: </span>${data.elements[i].largest_city}</li>
                         <li><span>State Bird: </span>${data.elements[i].state_bird}</li>
                         <li><span>State Flower: </span>${data.elements[i].state_flower}</li>
                         <li><span>Nickname: </span>${data.elements[i].nickname}</li>
-                        <li><span>Population: </span>${data.elements[i].population}</li>
-                        <li><span>Area (sq mi): </span>${data.elements[i].area_sq_mi}</li>
+                        <li><span>Population: </span>${formatNumber(data.elements[i].population)}</li>
+                        <li><span>Area (sq mi): </span>${formatNumber(data.elements[i].area_sq_mi)}</li>
                     </ul>
                 </div>
+            </div>
                 `
             dataDisplay.innerHTML = html;
             dataContainer.showModal()
