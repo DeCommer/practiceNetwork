00da76c03ff58d535fb39a35af0fccec238abaf5
Joseph DeCommer <jdecommer@gmail.com>
2024-07-14 15:06:00 -0400

    Added click counter

---
+++ js/clickcounter.js
@@ -0,0 +1,13 @@
+let countNum = document.getElementById('count-num');
+const enterBtn = document.getElementById('enter-btn');
+const clearBtn = document.getElementById('clear-btn');
+
+countNum.innerHTML = 0;
+
+enterBtn.addEventListener('click', () => {
+    countNum.innerHTML++;
+});
+
+clearBtn.addEventListener('click', () => {
+    countNum.innerHTML = 0;
+})
