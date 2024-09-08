a0a3dd9cec54c6d84855242ad859749ce0f45c97
Joseph DeCommer <jdecommer@gmail.com>
2024-09-08 12:23:29 -0400

    clean up

--- js/scripts.js
+++ js/scripts.js
@@ -15,23 +15,12 @@ setInterval(() =>{
 let url = window.location.href;
 console.log(url)
 
-console.log("local url");
-
 if(url.includes("10.0.0") || url.includes("127.0.0.1")) {
-  document.querySelector(".title").innerHTML = `Joseph DeCommer (Local)`;
+    document.querySelector(".title").innerHTML = `Joseph DeCommer (Local)`;
+    console.log("local");
+}else {
+    console.log('online');
 }
-
-// const modeBtn = document.querySelector('#mode-btn');
-// modeBtn.addEventListener('click', () => {
-//     const theme = document.getElementsByTagName('link')[0]; 
-//     if (theme.getAttribute('href') == './css/main.css') { 
-//         theme.setAttribute('href', './css/dark.css');
-//         modeBtn.innerHTML = '🌕';
-//     } else { 
-//         theme.setAttribute('href', './css/main.css');
-//         modeBtn.innerHTML = '🌑'; 
-//     } 
-// });
 
 var toggle = document.getElementById("theme-toggle");
 const icon = document.querySelector(".theme-icon");
