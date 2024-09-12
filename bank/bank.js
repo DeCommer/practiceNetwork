b905cb1267c1d999b86b611458c5622681f9b27e
Joseph DeCommer <jdecommer@gmail.com>
2024-09-12 15:21:26 -0400

    Added false accounts and features

--- bank/bank.js
+++ bank/bank.js
@@ -1,20 +1,8 @@
-fetch("./accounts.json")
-.then(response => response.json())
-.then(data => getAccounts(data));
-
-
-getAccounts = (data) => {
-    const accounts = data.accounts;
-    console.log(accounts);
-}
-
 const getDate = document.getElementById('date-time')
 const inputModal = document.querySelector('.input-modal');
 const overlay = document.querySelector(".overlay");
 const debitBtn = document.getElementById("debit-btn");
 const savingsBtn = document.getElementById("savings-btn");
-const accountNumber = Math.random().toString(36).slice(2);
-document.getElementById('account-number').innerHTML = `${accountNumber}`;
 
 const now = new Date();
 getDate.innerHTML=`Today is ${now.toLocaleString()}`;
@@ -35,7 +23,7 @@ function displayDebitModal() {
     inputModal.innerHTML=`
         <h3>Deposit into Debit</h3>
         <label>Amount: </label>
-        <input id="amt-in" type="number" placeholder="0">
+        <input id="amt-in" type="number" placeholder="0"  min="1">
         <button id="debit-enter-button" class="sm-btn btn">Enter</button>
         <a class="close-input-modal">x</a>
     `;
@@ -54,6 +42,7 @@ function addDebitFunds() {
     debitBalanceDisplay.innerHTML = `${moneyFormatter.format(debitBalance)}`;
     overlay.style.display = "none";
     inputModal.classList.add('hidden');
+    return debitBalance;
 };
 
 function displaySavingsModal() {
@@ -64,7 +53,7 @@ function displaySavingsModal() {
     inputModal.innerHTML=`
         <h3>Deposit into Savings</h3>
         <label>Amount: </label>
-        <input id="amt-in" type="number" placeholder="0">
+        <input id="amt-in" type="number" placeholder="0" min="1">
         <button id="savings-enter-button" class="sm-btn btn">Enter</button>
         <a class="close-input-modal">x</a>
     `;
@@ -83,12 +72,14 @@ function addSavingsFunds() {
     savingsBalanceDisplay.innerHTML = `${moneyFormatter.format(savingsBalance)}`;
     overlay.style.display = "none";
     inputModal.classList.add('hidden');
+    return savingsBalance;
 };
 
 function updtateTotalBalance() {
     const totalBalanceDisplay = document.getElementById("total-balance");
     let totalBalance = debitBalance + savingsBalance;
     totalBalanceDisplay.innerHTML = `${moneyFormatter.format(totalBalance)}`;
+    return totalBalance;
 };
 
 function closeModal() {
@@ -111,42 +102,77 @@ savingsBtn.addEventListener("click", (e) => {
 
 //Login / logout
 
-const userIn = document.getElementById("user-in");
+fetch("./accounts.json")
+.then(response => response.json())
+.then(data => getAccounts(data));
 
-function login() {
-    let userName = userIn.value;
-    if(passwordIn.value === '1234') {
-        document.querySelector(".login-apps-container").classList.add('hide'); 
-        document.querySelector(".apps-container").classList.remove('hide'); 
-        document.getElementById("greeting-txt").innerHTML = `${userName}`;
-    }else {
-        setTimeout(() => {
-            document.getElementById('message').innerHTML = ''
-        },1500);
-        document.getElementById('message').innerHTML = 'Invalid credentials';
-    }
-};
+getAccounts = (data) => {
+    const accounts = data.accounts;
+    console.log(accounts);
 
-const passwordIn = document.getElementById("pass-in");
+    // const accountNumber = Math.random().toString(36).slice(2);
+    const userIn = document.getElementById("user-in");
+    const passwordIn = document.getElementById("pass-in");
 
-passwordIn.addEventListener("keyup", function(e) {
-    if (e.key === "Enter") {
+    function login() {
+        let userName = userIn.value;
+
+        if(userName === accounts[0].username && passwordIn.value === accounts[0].password) {
+            document.querySelector(".login-apps-container").classList.add('hide'); 
+            document.querySelector(".apps-container").classList.remove('hide');
+            document.getElementById("greeting-txt").innerHTML = `${accounts[0].username}`;
+            document.getElementById('account-number').innerHTML = `${accounts[0].accountNumber}`;
+            savingsBalance = accounts[0].savingsBalance;
+            debitBalance = accounts[0].debitBalance;
+            savingsBalanceDisplay.innerHTML = `${moneyFormatter.format(accounts[0].savingsBalance)}`;
+            debitBalanceDisplay.innerHTML = `${moneyFormatter.format(accounts[0].debitBalance)}`;
+            updtateTotalBalance();
+        }else if(userName === accounts[1].username && passwordIn.value === accounts[1].password) {
+            document.querySelector(".login-apps-container").classList.add('hide'); 
+            document.querySelector(".apps-container").classList.remove('hide');
+            document.getElementById("greeting-txt").innerHTML = `${accounts[1].username}`;
+            document.getElementById('account-number').innerHTML = `${accounts[1].accountNumber}`;
+            savingsBalance = accounts[1].savingsBalance;
+            debitBalance = accounts[1].debitBalance;
+            savingsBalanceDisplay.innerHTML = `${moneyFormatter.format(accounts[1].savingsBalance)}`;
+            debitBalanceDisplay.innerHTML = `${moneyFormatter.format(accounts[1].debitBalance)}`;
+            updtateTotalBalance();
+        }else if(userName === accounts[2].username && passwordIn.value === accounts[2].password) {
+            document.querySelector(".login-apps-container").classList.add('hide'); 
+            document.querySelector(".apps-container").classList.remove('hide');
+            document.getElementById("greeting-txt").innerHTML = `${accounts[2].username}`;
+            document.getElementById('account-number').innerHTML = `${accounts[2].accountNumber}`;
+            savingsBalance = accounts[2].savingsBalance;
+            debitBalance = accounts[2].debitBalance;
+            savingsBalanceDisplay.innerHTML = `${moneyFormatter.format(accounts[2].savingsBalance)}`;
+            debitBalanceDisplay.innerHTML = `${moneyFormatter.format(accounts[2].debitBalance)}`;
+            updtateTotalBalance();
+        }else {
+            setTimeout(() => {
+                document.getElementById('message').innerHTML = ''
+            },1500);
+            document.getElementById('message').innerHTML = 'Invalid credentials';
+        }
+    };
+
+    passwordIn.addEventListener("keyup", function(e) {
+        if (e.key === "Enter") {
+            login();
+        }
+    });
+
+    const loginBtn = document.getElementById("login-btn");
+
+    loginBtn.addEventListener('click', () => {
         login();
-    }
-  });
+    });
 
-const loginBtn = document.getElementById("login-btn");
+    const logout = document.getElementById('logout');
+    logout.addEventListener('click', () => {
+        document.querySelector(".login-apps-container").classList.remove('hide'); 
+        document.querySelector(".apps-container").classList.add('hide'); 
+        userIn.value = "";
+        passwordIn.value = "";
+    });
 
-loginBtn.addEventListener('click', () => {
-    login();
-});
-
-const logout = document.getElementById('logout');
-logout.addEventListener('click', () => {
-    document.querySelector(".login-apps-container").classList.remove('hide'); 
-    document.querySelector(".apps-container").classList.add('hide'); 
-    userIn.value = "";
-    passwordIn.value = "";
-});
-
-
+}
