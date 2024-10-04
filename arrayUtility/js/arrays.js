47a00acaa86d6c35b2c81c556bd957f43ffe932c
Joseph DeCommer <jdecommer@gmail.com>
2024-10-04 11:25:29 -0400

    improvements

--- arrayUtility/js/arrays.js
+++ arrayUtility/js/arrays.js
@@ -6,18 +6,27 @@ const largestNum = document.getElementById("lrg-num-txt");
 const smallestNum = document.getElementById("sm-num-txt");
 const sumNum = document.getElementById("sum-num-txt");
 const avgNum = document.getElementById("avg-num-txt");
-
+let arrLength = document.getElementById("arr-len-in");
+let elementVal = document.getElementById("el-val-in");
 
 function randomIntArray() {
     let arrLength = document.getElementById("arr-len-in").value;
+    let elementVal = document.getElementById("el-val-in").value;
 	let randomArray = new Array(arrLength);
 	for(let i = 0; i < arrLength; i++) {
-		let randValue = Math.floor(Math.random() * 499) + 1;
+		let randValue = Math.floor(Math.random() * elementVal) + 1;
 		randomArray[i] = randValue;
 	}
 	return randomArray;
 }
 
+function arrayDisplay(arr) {
+    let textArr = new Array(arrLength);
+    for(let i = 0; i < arr.length; i++) {
+        textArr[i] = " " + arr[i].toLocaleString('en-US');
+    }
+    return textArr.join("");
+}
 
 function largestNumberInArray(arr) {
 	let maxVal = arr[0];
@@ -49,24 +58,49 @@ function sumNumbersInArray(arr) {
 
 function guts() {
     let arr = randomIntArray();
-    array.innerHTML = `
-    <div class="array-area">
-    <span class="arrSpan">${arr}</span>
-    </div>
-    `
-    arrLenTxt.innerHTML = `Array length: ${arr.length}`;
-    largestNum.innerHTML =`Largest number: ${largestNumberInArray(arr)}`;
-    smallestNum.innerHTML =`Smallest number: ${smallestNumberInArray(arr)}`;
-    sumNum.innerHTML = `Sum: ${sumNumbersInArray(arr)}`;
-    avgNum.innerHTML = `Average: ${(sumNumbersInArray(arr)/arr.length).toFixed(1)}`;
-    console.log(arr);
+    if(arr.length === 1 || elementVal === 1) {
+        clear();
+    } else {
+        array.innerHTML = `
+        <div class="array-area">
+        <span class="arrSpan">${arrayDisplay(arr)}</span>
+        </div>
+        `
+        arrLenTxt.innerHTML = `Array length: <span class="num-text">${arr.length}</span>`;
+        largestNum.innerHTML =`Largest number: <span class="num-text">${largestNumberInArray(arr).toLocaleString('en-US')}</span>`;
+        smallestNum.innerHTML =`Smallest number: <span class="num-text">${smallestNumberInArray(arr).toLocaleString('en-US')}</span>`;
+        sumNum.innerHTML = `Sum: <span class="num-text">${sumNumbersInArray(arr).toLocaleString('en-US')}</span>`;
+        avgNum.innerHTML = `Average: <span class="num-text">${(sumNumbersInArray(arr)/arr.length).toLocaleString('en-US')}</span>`;
+        // console.log(arr.length);
+    }
+}
+
+function clear() {
+    array.innerHTML = `Enter value`;
+    setTimeout(() =>{
+        array.innerHTML = ``;
+    }, 1500);
+    arrLength.value = '';
+    arrLenTxt.innerHTML = ``;
+    largestNum.innerHTML =``;
+    smallestNum.innerHTML =``;
+    sumNum.innerHTML = ``;
+    avgNum.innerHTML = ``;
 }
 
 enterBtn.addEventListener('click', () => {
     guts();
-
 });
 
+arrLength.addEventListener('keypress', (e) => {if(e.key == 'Enter')guts()});
+elementVal.addEventListener('keypress', (e) => {if(e.key == 'Enter')guts()});
+
 clearBtn.addEventListener('click', () => {
-    array.innerHTML = ``
+    array.innerHTML = ``;
+    arrLength.value = '';
+    arrLenTxt.innerHTML = ``;
+    largestNum.innerHTML =``;
+    smallestNum.innerHTML =``;
+    sumNum.innerHTML = ``;
+    avgNum.innerHTML = ``;
 });
