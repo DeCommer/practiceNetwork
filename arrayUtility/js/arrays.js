0695998a69fca2deb4b541363b00467b781da895
Joseph DeCommer <jdecommer@gmail.com>
2024-10-04 10:06:38 -0400

    added app

--- arrayUtility/js/arrays.js
+++ arrayUtility/js/arrays.js
@@ -1,9 +1,14 @@
 const array = document.getElementById("array");
 const enterBtn = document.getElementById("enter-btn");
+const arrLenTxt =  document.getElementById("arr-length-txt");
 const clearBtn = document.getElementById("clear-btn");
+const largestNum = document.getElementById("lrg-num-txt");
+const smallestNum = document.getElementById("sm-num-txt");
+const sumNum = document.getElementById("sum-num-txt");
+const avgNum = document.getElementById("avg-num-txt");
 
 
-function randonmIntArray() {
+function randomIntArray() {
     let arrLength = document.getElementById("arr-len-in").value;
 	let randomArray = new Array(arrLength);
 	for(let i = 0; i < arrLength; i++) {
@@ -14,10 +19,52 @@ function randonmIntArray() {
 }
 
 
+function largestNumberInArray(arr) {
+	let maxVal = arr[0];
+	for(let i = 1; i < arr.length; i++) {
+		if(arr[i] > maxVal) {
+			maxVal = arr[i];
+		}
+	}
+	return maxVal;
+}
 
+function smallestNumberInArray(arr) {
+	let maxVal = arr[0];
+	for(let i = 1; i < arr.length; i++) {
+		if(arr[i] < maxVal) {
+			maxVal = arr[i];
+		}
+	}
+	return maxVal;
+}
+
+function sumNumbersInArray(arr) {
+	let sum = 0;
+	for(let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	return sum;
+}
+
+function guts() {
+    let arr = randomIntArray();
+    array.innerHTML = `
+    <div class="array-area">
+    <span class="arrSpan">${arr}</span>
+    </div>
+    `
+    arrLenTxt.innerHTML = `Array length: ${arr.length}`;
+    largestNum.innerHTML =`Largest number: ${largestNumberInArray(arr)}`;
+    smallestNum.innerHTML =`Smallest number: ${smallestNumberInArray(arr)}`;
+    sumNum.innerHTML = `Sum: ${sumNumbersInArray(arr)}`;
+    avgNum.innerHTML = `Average: ${(sumNumbersInArray(arr)/arr.length).toFixed(1)}`;
+    console.log(arr);
+}
 
 enterBtn.addEventListener('click', () => {
-    array.innerHTML = `<b>Array:</b> <span>${randonmIntArray(array)}</span>`
+    guts();
+
 });
 
 clearBtn.addEventListener('click', () => {
