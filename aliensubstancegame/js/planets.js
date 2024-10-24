c560d5bb95486ad1eb527586d2c1156fcc74808b
Joseph DeCommer <jdecommer@gmail.com>
2024-10-24 17:43:01 -0400

    removed some images

--- aliensubstancegame/js/planets.js
+++ aliensubstancegame/js/planets.js
@@ -9,11 +9,13 @@ export default function planets() {
     const placeBtn_1 = document.getElementById("place-btn-1");
     const placeBtn_2 = document.getElementById("place-btn-2");
     const placeBtn_3 = document.getElementById("place-btn-3");
-    const placeBtn_4 = document.getElementById("place-btn-4");
-    const placeBtn_5 = document.getElementById("place-btn-5");
     const spaceshipBtn = document.getElementById("place-btn-ship");
     const spaceshipModal = document.getElementById("space-ship-modal");
     const overlay = document.querySelector(".overlay");
+
+    const planet0Btn = document.getElementById("planet0");
+    const planet1Btn = document.getElementById("planet1");
+    const planet2Btn = document.getElementById("planet2");
 
     function displaySpaceshipModal() {
         overlay.style.display = "block";
@@ -35,6 +37,7 @@ export default function planets() {
 
     function displayPlanets(planetData) {
         let currentPlanet = planetData.planets[0];
+        planetSwitch();
 
         placeBtn_0.addEventListener('click', () => {
             message.innerHTML = `<p>You have traveled to ${planetData.planets[0].sectors[0]}</P>`;
@@ -43,8 +46,6 @@ export default function planets() {
             placeBtn_1.classList.remove("active");
             placeBtn_2.classList.remove("active");
             placeBtn_3.classList.remove("active");
-            placeBtn_4.classList.remove("active");
-            placeBtn_5.classList.remove("active");
         });
         placeBtn_1.addEventListener('click', () => {
             message.innerHTML = `<p>You have traveled to ${planetData.planets[0].sectors[1]}</P>`;
@@ -53,8 +54,6 @@ export default function planets() {
             placeBtn_1.classList.add("active");
             placeBtn_2.classList.remove("active");
             placeBtn_3.classList.remove("active");
-            placeBtn_4.classList.remove("active");
-            placeBtn_5.classList.remove("active");
         });
         placeBtn_2.addEventListener('click', () => {
             message.innerHTML = `<p>You have traveled to ${planetData.planets[0].sectors[2]}</P>`;
@@ -63,8 +62,6 @@ export default function planets() {
             placeBtn_1.classList.remove("active");
             placeBtn_2.classList.add("active");
             placeBtn_3.classList.remove("active");
-            placeBtn_4.classList.remove("active");
-            placeBtn_5.classList.remove("active");
         });
         placeBtn_3.addEventListener('click', () => {
             message.innerHTML = `<p>You have traveled to ${planetData.planets[0].sectors[3]}</P>`;
@@ -73,28 +70,6 @@ export default function planets() {
             placeBtn_1.classList.remove("active");
             placeBtn_2.classList.remove("active");
             placeBtn_3.classList.add("active");
-            placeBtn_4.classList.remove("active");
-            placeBtn_5.classList.remove("active");
-        });
-        placeBtn_4.addEventListener('click', () => {
-            message.innerHTML = `<p>You have traveled to ${planetData.planets[0].sectors[4]}</P>`;
-            sectorNameText.innerHTML = planetData.planets[0].sectors[4];
-            placeBtn_0.classList.remove("active");
-            placeBtn_1.classList.remove("active");
-            placeBtn_2.classList.remove("active");
-            placeBtn_3.classList.remove("active");
-            placeBtn_4.classList.add("active");
-            placeBtn_5.classList.remove("active");
-        });
-        placeBtn_5.addEventListener('click', () => {
-            message.innerHTML = `<p>You have traveled to ${planetData.planets[0].sectors[5]}</P>`
-            sectorNameText.innerHTML = planetData.planets[0].sectors[5];
-            placeBtn_0.classList.remove("active");
-            placeBtn_1.classList.remove("active");
-            placeBtn_2.classList.remove("active");
-            placeBtn_3.classList.remove("active");
-            placeBtn_4.classList.remove("active");
-            placeBtn_5.classList.add("active");
         });
 
         spaceshipBtn.addEventListener('click', () => {
@@ -102,35 +77,51 @@ export default function planets() {
             closeModal();
         });
 
-        if(currentPlanet === planetData.planets[0]) {
-            planetNameText.innerHTML = planetData.planets[0].name;
-            sectorNameText.innerHTML = planetData.planets[0].sectors[0];
-            placeBtn_0.innerHTML = planetData.planets[0].sectors[0];
-            placeBtn_1.innerHTML = planetData.planets[0].sectors[1];
-            placeBtn_2.innerHTML = planetData.planets[0].sectors[2];
-            placeBtn_3.innerHTML = planetData.planets[0].sectors[3];
-            placeBtn_4.innerHTML = planetData.planets[0].sectors[4];
-            placeBtn_5.innerHTML = planetData.planets[0].sectors[5];
-        }else if(currentPlanet === planetData.planets[1]) {
-            planetNameText.innerHTML = planetData.planets[1].name;
-            sectorNameText.innerHTML = planetData.planets[1].sectors[0];
-    
-            placeBtn_0.innerHTML = planetData.planets[1].sectors[0];
-            placeBtn_1.innerHTML = planetData.planets[1].sectors[1];
-            placeBtn_2.innerHTML = planetData.planets[1].sectors[2];
-            placeBtn_3.innerHTML = planetData.planets[1].sectors[3];
-            placeBtn_4.innerHTML = planetData.planets[1].sectors[4];
-            placeBtn_5.innerHTML = planetData.planets[1].sectors[5];
-        }else if(currentPlanet === planetData.planets[2]) {
-            planetNameText.innerHTML = planetData.planets[2].name;
-            sectorNameText.innerHTML = planetData.planets[2].sectors[0];
-    
-            placeBtn_0.innerHTML = planetData.planets[2].sectors[0];
-            placeBtn_1.innerHTML = planetData.planets[2].sectors[1];
-            placeBtn_2.innerHTML = planetData.planets[2].sectors[2];
-            placeBtn_3.innerHTML = planetData.planets[2].sectors[3];
-            placeBtn_4.innerHTML = planetData.planets[2].sectors[4];
-            placeBtn_5.innerHTML = planetData.planets[2].sectors[5];
+        planet0Btn.addEventListener('click', () => {
+            currentPlanet = planetData.planets[0];
+            planetSwitch();
+            overlay.style.display = "none";
+            spaceshipModal.classList.add('hide');
+        });
+        planet1Btn.addEventListener('click', () => {
+            currentPlanet = planetData.planets[1];
+            planetSwitch();
+            overlay.style.display = "none";
+            spaceshipModal.classList.add('hide');
+        });
+        planet2Btn.addEventListener('click', () => {
+            currentPlanet = planetData.planets[2];
+            planetSwitch();
+            overlay.style.display = "none";
+            spaceshipModal.classList.add('hide');
+        });
+
+        function planetSwitch() {
+            if(currentPlanet === planetData.planets[0]) {
+                planetNameText.innerHTML = planetData.planets[0].name;
+                sectorNameText.innerHTML = planetData.planets[0].sectors[0]; 
+                placeBtn_0.innerHTML = planetData.planets[0].sectors[0];
+                placeBtn_1.innerHTML = planetData.planets[0].sectors[1];
+                placeBtn_2.innerHTML = planetData.planets[0].sectors[2];
+                placeBtn_3.innerHTML = planetData.planets[0].sectors[3];
+
+            }else if(currentPlanet === planetData.planets[1]) {
+                planetNameText.innerHTML = planetData.planets[1].name;
+                sectorNameText.innerHTML = planetData.planets[1].sectors[0];
+                placeBtn_0.innerHTML = planetData.planets[1].sectors[0];
+                placeBtn_1.innerHTML = planetData.planets[1].sectors[1];
+                placeBtn_2.innerHTML = planetData.planets[1].sectors[2];
+                placeBtn_3.innerHTML = planetData.planets[1].sectors[3];
+
+            }else if(currentPlanet === planetData.planets[2]) {
+                planetNameText.innerHTML = planetData.planets[2].name;
+                sectorNameText.innerHTML = planetData.planets[2].sectors[0];
+                placeBtn_0.innerHTML = planetData.planets[2].sectors[0];
+                placeBtn_1.innerHTML = planetData.planets[2].sectors[1];
+                placeBtn_2.innerHTML = planetData.planets[2].sectors[2];
+                placeBtn_3.innerHTML = planetData.planets[2].sectors[3];
+
+            }
         }
     }
 }
